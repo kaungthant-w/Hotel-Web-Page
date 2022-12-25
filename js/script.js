7bc96bec0e6f1a27c569825072e9e58b2e64cf36
“kaungthant-w” <kaungthant9513@gmail.com>
2022-12-25 12:19:04 +0630

    menu btn

--- js/script.js
+++ js/script.js
@@ -2,10 +2,14 @@ let menContainer = document.querySelector("#menuContainer");
 let menBtn = document.querySelector("#menuBtn");
 let overlay = document.querySelector("#overlay");
 let menuItem = document.querySelectorAll("a");
+let menuIconOpen = document.querySelector("#menuIconOpen");
+let menuIconClose = document.querySelector("#menuIconClose");
 
 let toggleMenu = function() {
     menContainer.classList.toggle("hidden");
     overlay.classList.toggle("hidden");
+    menuIconOpen.classList.toggle("hidden");
+    menuIconClose.classList.toggle("hidden");
 }
 
 
